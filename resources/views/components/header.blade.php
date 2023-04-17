6c253a00e322b6b6aa6d268417eadadc32bf7c57
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-04-17 09:23:06 +0300

    init commit

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,25 @@
+<header>
+    <div class="containers header">
+        <div class="header__content">
+            <div class="header__logo">
+                <a href="{{ route('index') }}">
+                    <img src="{{ asset('assets/img/logo.svg') }}" alt="">
+                </a>
+            </div>
+            <div class="header__nav">
+                <ul class="nav nav__list-header">
+                    <li class="nav__item-header">
+                        <a href="#about">
+                            О компании
+                        </a>
+                    </li>
+                    <li class="nav__item-header"><a href="#services">Услуги</a></li>
+                    <li class="nav__item-header"><a href="#stages">Этапы работы</a></li>
+                    <li class="nav__item-header"><a href="#certificate">Сертификаты</a></li>
+                    <li class="nav__item-header"><a href="#contacts">Контакты</a></li>
+                </ul>
+            </div>
+            <a class="header__phone" href="tel:+7 831 235-33-69">+7 831 235-33-69</a>
+        </div>
+    </div>
+</header>
