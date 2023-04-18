8ac1b0d84062989f0cafd775ec5f033ac792157a
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-04-18 16:00:12 +0300

    burger fixed

--- resources/views/sections/about.blade.php
+++ resources/views/sections/about.blade.php
@@ -1,23 +1,35 @@
  <section id="about">
-     <div class="about__content containers">
+     <div class="about containers">
 
-         <div class="about__text">
-             <h2>О предприятии</h2>
-             <p>Наша баня работает с 2010 года. У нас вы можете окунуться в настоящую русскую баньку, попариться и
-                 расслабиться. Также мы можем организовать для вас банный день или банный квест. Если вы хотите устроить
-                 праздник - обращайтесь к нам.
-                 <br>
-                  <br>
-                 Наши цены вас приятно удивят. Вы можете выбрать парную на свой вкус. Если вас двое, то мы предлагаем
-                 паровую баню.
+         <div class="about__content ">
 
-                 <br> 
-                 <br>
-                 Мы поможем вам организовать праздник для детей и взрослых.
-             </p>
-         </div>
-         <div class="about__image">
-             <img src="{{ asset('assets/img/about-img.png') }}" alt="">
+             <div class="about__text">
+                 <h2>О предприятии</h2>
+                 <p>Наша баня работает с 2010 года. У нас вы можете окунуться в настоящую русскую баньку, попариться и
+                     расслабиться. Также мы можем организовать для вас банный день или банный квест. Если вы хотите
+                     устроить
+                     праздник - обращайтесь к нам.
+                     <br>
+                     <br>
+                     Наши цены вас приятно удивят. Вы можете выбрать парную на свой вкус. Если вас двое, то мы
+                     предлагаем
+                     паровую баню.
+
+                     <br>
+                     <br>
+                     Мы поможем вам организовать праздник для детей и взрослых.
+                 </p>
+             </div>
+             <div class="about__image">
+                 <div class="about__image-col-1">
+                     <img style="margin-bottom: 10px" src="{{ asset('assets/img/about1.jpg') }}" alt="">
+                     <img src="{{ asset('assets/img/about2.jpg') }}" alt="">
+                 </div>
+                 <div class="about__image-col-2">
+                     <img style="margin-bottom: 10px" src="{{ asset('assets/img/about3.jpg') }}" alt="">
+                     <img src="{{ asset('assets/img/about4.jpg') }}" alt="">
+                 </div>
+             </div>
          </div>
      </div>
  </section>
