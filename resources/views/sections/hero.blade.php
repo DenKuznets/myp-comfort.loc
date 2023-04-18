ed0509de9b3e09254bb74eed46d33a57b1a73cb6
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-04-18 10:32:55 +0300

    hero done

--- resources/views/sections/hero.blade.php
+++ resources/views/sections/hero.blade.php
@@ -1,14 +1,9 @@
-<section class="section" id="hero">
-  <div class="hero__content">
-    <div class="hero__text">
-      <h1 class="">Оформление переоборудования транспортных средств в ГИБДД</h1>
-      <p class="">Мы поможем вам подготовить документы для регистрации изменений в конструкцию вашего ТС,
-        или переоборудование ТС в ГИБДД
-        на законных основаниях.</p>
-      <button id="popAp" class="btn btn-cta">Оставить заявку</button>
+<section id="hero">
+    <div class="hero containers">
+        <div class="hero__content">
+            <div class="hero__text">
+                <h1 class=""><span>Отдыхайте</span> <br>  с комфортом</h1>
+            </div>
+        </div>
     </div>
-    <div class="hero__image">
-      <img src="{{ asset('assets/img/hero-img.png') }}" alt="">
-    </div>
-  </div>
 </section>
