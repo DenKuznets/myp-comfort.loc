d919ca0ec9c69df9860a3a2393019fb46c2be7f7
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-04-19 15:00:34 +0300

    info done

---
+++ resources/views/sections/info.blade.php
@@ -0,0 +1,37 @@
+<section id="info">
+    <div class="info containers">
+        <h2>Важная информация</h2>
+        <div class="info__cards-container">
+            <div class=" info__important-card">
+                <div class="info__important-card-img">
+                    {{-- <img src="{{ asset('assets/img/important-img.jpg') }}" alt=""> --}}
+                    <picture>
+                        <source srcset="{{ asset('assets/img/important1920.jpg') }}" media="(min-width: 1025px)">
+                        <source srcset="{{ asset('assets/img/important1024.jpg') }}" media="(max-width: 1024px)">
+                        <source srcset="{{ asset('assets/img/important768.jpg') }}" media="(max-width: 768px)">
+                        <source srcset="{{ asset('assets/img/important480.jpg') }}" media="(max-width: 480px)">
+                        <source srcset="{{ asset('assets/img/important360.jpg') }}" media="(max-width: 360px)">
+                        <img src="{{ asset('assets/img/important-img.jpg') }}" alt="">
+                    </picture>
+                </div>
+                <div class="info__card info__card--important">
+                    <h4>Мы не работаем 15 марта!</h4>
+                    <p>Наша баня не работает в данный момент из-за технических неполадок</p>
+                    <a href="" class="info__details">Подробнее</a>
+                </div>
+            </div>
+            <div class="info__cards">
+                <div class="info__card">
+                    <p class="info__card-p-bold">График работы на 8 марта</p>
+                    <p>Мы работаем для вас с 10:00 до 19:00! Если у вас намечается большое застолье,</p>
+                    <a href="" class="info__details">Подробнее</a>
+                </div>
+                <div class="info__card">
+                    <p class="info__card-p-bold">Ищем нового сотрудника</p>
+                    <p>Ищем сотрудника в баню на постоянную работу в центре города. Требования: Опыт работы…</p>
+                    <a href="" class="info__details">Подробнее</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
