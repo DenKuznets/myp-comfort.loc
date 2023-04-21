4b1587a869fe95905ec1dadf883ccc45542cbc7c
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-04-21 14:44:28 +0300

    info final

--- resources/views/sections/info.blade.php
+++ resources/views/sections/info.blade.php
@@ -7,10 +7,10 @@
                     {{-- <img src="{{ asset('assets/img/important-img.jpg') }}" alt=""> --}}
                     <picture>
                         <source srcset="{{ asset('assets/img/important1920.jpg') }}" media="(min-width: 1025px)">
-                        <source srcset="{{ asset('assets/img/important1024.jpg') }}" media="(max-width: 1024px)">
-                        <source srcset="{{ asset('assets/img/important768.jpg') }}" media="(max-width: 768px)">
-                        <source srcset="{{ asset('assets/img/important480.jpg') }}" media="(max-width: 480px)">
-                        <source srcset="{{ asset('assets/img/important360.jpg') }}" media="(max-width: 360px)">
+                        <source srcset="{{ asset('assets/img/important1024.jpg') }}" media="(min-width: 1024px)">
+                        <source srcset="{{ asset('assets/img/important768.jpg') }}" media="(min-width: 768px)">
+                        <source srcset="{{ asset('assets/img/important480.jpg') }}" media="(min-width: 480px)">
+                        <source srcset="{{ asset('assets/img/important360.jpg') }}" media="(min-width: 360px)">
                         <img src="{{ asset('assets/img/important-img.jpg') }}" alt="">
                     </picture>
                 </div>
