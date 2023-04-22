82d5a36f7aef21b3b6b5a6085c789e61a1d898fc
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-04-22 15:27:14 +0300

    popups done

--- resources/src/js/components/pop-ap-services.js
+++ resources/src/js/components/pop-ap-services.js
@@ -1,15 +1,11 @@
 (function () {
     const buttons = document.querySelectorAll("#popUp");
-    // попап в секции бани (#bath2, #bath3)
-    // const modalForm = document.getElementById('bathPopUp')
-    const close = document.querySelector(".closeForm");
 
     function openModal(modalForm) {
-        // console.log('open modal');
         modalForm.style.display = "block";
         document.body.classList.add("lock");
+        const close = modalForm.querySelector(".closeForm");
         close.onclick = () => {
-            console.log('close btn');
             modalForm.style.display = "none";
             document.body.classList.remove("lock");
         };
@@ -30,19 +26,18 @@
                 case "bath3":
                     openModal(document.getElementById("bathPopUp"));
                     break;
-                case 'rooms':
-                    if (e.target.closest(".rooms__card").id === 'room1') {
+                case "rooms":
+                    if (e.target.closest(".rooms__card").id === "room1") {
                         openModal(document.getElementById("room1PopUp"));
                     } else {
                         openModal(document.getElementById("room2PopUp"));
                     }
                     break;
-                case 'laundry':
-                    console.log('laundry popup');
-                    
+                case "laundry":
+                    openModal(document.getElementById("laundryPopUp"));
                     break;
                 default:
-                    console.log('no such section');
+                    console.log("no such section");
                     break;
             }
         });
