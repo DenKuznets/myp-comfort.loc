d59fe37b0fd99cee1ed57ddbd62bc8b36eacd9cf
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-04-24 15:58:38 +0300

    1

--- resources/views/sections/about.blade.php
+++ resources/views/sections/about.blade.php
@@ -1,8 +1,6 @@
  <section id="about">
      <div class="about containers">
-
          <div class="about__content ">
-
              <div class="about__text animate__animated">
                  <h2>О предприятии</h2>
                  <p>Наша баня работает с 2010 года. У нас вы можете окунуться в настоящую русскую баньку, попариться и
@@ -21,12 +19,14 @@
                  </p>
              </div>
              <div class="about__image animate__animated">
-                 <div class="about__image-col-1">                    
-                     <img class="about__img1" style="margin-bottom: 10px" src="{{ asset('assets/img/about1.jpg') }}" alt="">
+                 <div class="about__image-col-1">
+                     <img class="about__img1" style="margin-bottom: 10px" src="{{ asset('assets/img/about1.jpg') }}"
+                         alt="">
                      <img class="about__img2" src="{{ asset('assets/img/about2.jpg') }}" alt="">
                  </div>
                  <div class="about__image-col-2">
-                     <img class="about__img3" style="margin-bottom: 10px" src="{{ asset('assets/img/about3.jpg') }}" alt="">
+                     <img class="about__img3" style="margin-bottom: 10px" src="{{ asset('assets/img/about3.jpg') }}"
+                         alt="">
                      <img class="about__img4" src="{{ asset('assets/img/about4.jpg') }}" alt="">
                  </div>
              </div>
