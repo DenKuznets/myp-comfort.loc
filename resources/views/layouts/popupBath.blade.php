9181108fc5eec49a130a5492d82af9dc97a24532
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-04-26 21:32:37 +0300

    edits done

--- resources/views/layouts/popupBath.blade.php
+++ resources/views/layouts/popupBath.blade.php
@@ -1,5 +1,5 @@
 <div id="bathPopUp" class="modalForm">
-    <div class="modal-content-form baths__popup">
+    <div class="modal-content-form baths__popup popup">
         <span class="closeForm">
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
@@ -7,56 +7,45 @@
                     fill="#C4CDDE" />
             </svg>
         </span>
+
         <h2>НАШИ БАНИ</h2>
-        <table>
-            <tr>
-                <th></th>
-                <th>Рабочие дни недели</th>
-                <th>Выходные и праздничные дни</th>
-            </tr>
-            <tr>
-                <td></td>
-                <td>310 руб/ 2-часовой сеанс</td>
-                <td>375 руб/ 2-часовой сеанс</td>
-            </tr>
-            <tr>
-                <td>Пенсионеры</td>
-                <td>280 руб/ 2-часовой сеанс</td>
-                <td>375 руб/ 2-часовой сеанс</td>
-            </tr>
-            <tr>
-                <td>Льготная категория*</td>
-                <td>220 руб/ 2-часовой сеанс</td>
-                <td>375 руб/ 2-часовой сеанс</td>
-            </tr>
-            <tr>
-                <td>Многодетные семьи; дети с 7 до 14 лет</td>
-                <td>220 руб/ 2-часовой сеанс</td>
-                <td>220 руб/ 2-часовой сеанс</td>
-            </tr>
-            <tr>
-                <td>Дети до 7 лет в сопровождении взрослого</td>
-                <td>БЕСПЛАТНО</td>
-                <td>БЕСПЛАТНО</td>
-            </tr>
-        </table>
-        <p>*К льготной категории относятся: участники, инвалиды ВОВ, вдовы участников ВОВ и лица к ним приравненные; инвалиды 1, 2 групп; труженики тыла;</p>
-
-
-
-        {{-- мишина форма обратной связи, не удаляй, вдруг понадобится --}}
-        {{-- <h3 class="modal-title">Оставьте заявку, мы обязательно свяжемся с вами!</h3>
-        <p class="modal-text">Оставьте свой номер телефона, и мы поможем воплотить вашу мечту в реальность</p>
-        <form class="modal-form">
-            <input class="modal-form-input" type="text" name="name" placeholder="Имя" id="inputName" required>
-            <input class="modal-form-input" type="text" name="phone" placeholder="Телефон" id="inputPhone" required>
-            <div class="modal-checkbox">
-                <input type="checkbox" id="checkbox">
-                <label>Согласен на обработку персональный данных</label>
-            </div>
-            <div class="modal-button">
-                <button class="btn btn-cta" id="sendMail" disabled>Оставить заявку</button>
-            </div>
-        </form> --}}
+        <div class="table-container">
+            <table>
+                <tr>
+                    <th></th>
+                    <th>Рабочие дни недели</th>
+                    <th>Выходные и праздничные дни</th>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>310 руб/ 2-часовой сеанс</td>
+                    <td>375 руб/ 2-часовой сеанс</td>
+                </tr>
+                <tr>
+                    <td>Пенсионеры</td>
+                    <td>280 руб/ 2-часовой сеанс</td>
+                    <td>375 руб/ 2-часовой сеанс</td>
+                </tr>
+                <tr>
+                    <td>Льготная категория*</td>
+                    <td>220 руб/ 2-часовой сеанс</td>
+                    <td>375 руб/ 2-часовой сеанс</td>
+                </tr>
+                <tr>
+                    <td>Многодетные семьи; дети с 7 до 14 лет</td>
+                    <td>220 руб/ 2-часовой сеанс</td>
+                    <td>220 руб/ 2-часовой сеанс</td>
+                </tr>
+                <tr>
+                    <td>Дети до 7 лет в сопровождении взрослого</td>
+                    <td>БЕСПЛАТНО</td>
+                    <td>БЕСПЛАТНО</td>
+                </tr>
+            </table>
+        </div>
+        <p>*К льготной категории относятся: участники, инвалиды ВОВ, вдовы участников ВОВ и лица к ним приравненные;
+            инвалиды 1, 2 групп; труженики тыла;</p>
     </div>
+
+
 </div>
