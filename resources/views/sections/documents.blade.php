34d76f191b6a0164c153714ad36303552dccbb3a
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-04-28 15:07:39 +0300

    documents refactored

--- resources/views/sections/documents.blade.php
+++ resources/views/sections/documents.blade.php
@@ -1,7 +1,22 @@
     <section id="documents">
-      <div class="documents containers">
+        <div class="documents containers">
+            <h2>Документы</h2>
+            <div class="documents__cards">
+                <div class="documents__card">
+                  <div class="documents__card-overlay"></div>
+                    <a href="">
+                        <img src="{{ asset('assets/img/doc-2-new.jpg') }}" alt="">
+                        <div class="documents__card-hover-text">Перейти к документу</div>
+                    </a>
+                </div>
+                <div class="documents__card">
+                  <div class="documents__card-overlay"></div>
+                    <a href="">
+                        <img src="{{ asset('assets/img/doc-1-new.jpg') }}" alt="">
+                        <div class="documents__card-hover-text">Перейти к документу</div>
+                    </a>
+                </div>
+            </div>
 
-        <h2>Документы</h2>
-        
-      </div>
+        </div>
     </section>
