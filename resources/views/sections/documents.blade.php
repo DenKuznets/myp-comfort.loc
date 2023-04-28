815d7a40bb3364980d864fa9ad0dd30ce4557871
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-04-28 14:39:23 +0300

    documents swiper deleted

--- resources/views/sections/documents.blade.php
+++ resources/views/sections/documents.blade.php
@@ -2,38 +2,6 @@
       <div class="documents containers">
 
         <h2>Документы</h2>
-        <div class="docsSwiper swiper">
-          <!-- Additional required wrapper -->
-          <div class="swiper-wrapper">
-            <!-- Slides -->
-            <div class="swiper-slide documents__slide">
-              <div class="documents__card">
-                <img src="{{ asset('assets/img/doc1.jpg') }}" alt="">
-              </div>
-            </div>
-            <div class="swiper-slide">
-              <div class="documents__card">
-                <img src="{{ asset('assets/img/doc2.jpg') }}" alt="">
-              </div>
-            </div>
-            <div class="swiper-slide">
-              <div class="documents__card">
-                <img src="{{ asset('assets/img/doc3.jpg') }}" alt="">
-              </div>
-            </div>
-            <div class="swiper-slide">
-              <div class="documents__card">
-                <img src="{{ asset('assets/img/doc4.jpg') }}" alt="">
-              </div>
-            </div>
-            
-          </div>
-          <!-- If we need navigation buttons -->
-          <!-- <div class="swiper-button-prev green"></div>
-          <div class="swiper-button-next green"></div> -->
-
-          <!-- If we need scrollbar -->
-          <!-- <div class="swiper-scrollbar"></div> -->
-        </div>
+        
       </div>
     </section>
